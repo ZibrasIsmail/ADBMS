71134c5d0d4f4e66e0eb58265d14d0dc4c5a2e5e
Famith Rosni <72956250+Famith-Rosni@users.noreply.github.com>
2021-11-28 21:07:54 +0530

    Update event.js

--- Server/routes/event.js
+++ Server/routes/event.js
@@ -1,27 +1,27 @@
 
-// const express = require('express')
-// const router = express.Router();
+const express = require('express')
+const router = express.Router();
 
 
-// const { 
-//     getEvent, 
-//     newEvent, 
-//     getSingleEvent, 
-//     updateEvent, 
-//     deleteEvent
+const { 
+    getEvent, 
+    newEvent, 
+    getSingleEvent, 
+    updateEvent, 
+    deleteEvent
 
-// } = require('../controllers/eventController')
+} = require('../controllers/eventController')
 
-// const parser = require("../middleware/cloudinary.config");
+const parser = require("../middleware/cloudinary.config");
 
-// router.post('/event/new', parser.single("image"), newEvent);
+router.post('/event/new', parser.single("image"), newEvent);
 
-// router.route('/event').get(getEvent);
+router.route('/event').get(getEvent);
 
-// router.route('/event/:id').get(getSingleEvent);
+router.route('/event/:id').get(getSingleEvent);
 
-// router.put('/event/:id', parser.single("image"), updateEvent);
+router.put('/event/:id', parser.single("image"), updateEvent);
 
-// router.route('/event/:id').delete(deleteEvent);
+router.route('/event/:id').delete(deleteEvent);
 
-// module.exports = router;
+module.exports = router;
