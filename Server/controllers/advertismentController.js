408adc578b9bb67e65d65f4e5a5a751981f7fcaa
AdshayaniPirapaharan <69112755+AdshayaniPirapaharan@users.noreply.github.com>
2021-11-28 22:13:14 +0530

    Update advertismentController.js

--- Server/controllers/advertismentController.js
+++ Server/controllers/advertismentController.js
@@ -1,91 +1,91 @@
-// const Advertisement = require('../models/advertisement')
+const Advertisement = require('../models/advertisement')
 
-// exports.newAdvertisement = async (req, res) => {
-//     const advertisement = new Advertisement({
-//         ad_name: req.body.ad_name,
-//         creatorId: req.body.creatorId,
-//         image: req.file.path,
-//     });
+exports.newAdvertisement = async (req, res) => {
+    const advertisement = new Advertisement({
+        ad_name: req.body.ad_name,
+        creatorId: req.body.creatorId,
+        image: req.file.path,
+    });
   
-//     try {
-//       await advertisement.save();
-//     } catch (error) {
-//       return res.status(201).json({
-//         message: `Advertisment Add failed, check to see the ${error}`,
-//         status: "error",
-//       });
-//     }
-//   };
+    try {
+      await advertisement.save();
+    } catch (error) {
+      return res.status(201).json({
+        message: `Advertisment Add failed, check to see the ${error}`,
+        status: "error",
+      });
+    }
+  };
 
-// exports.getAdvertisement = async (req, res) => {
+exports.getAdvertisement = async (req, res) => {
 
-//     const advertisement = await Advertisement.find();
+    const advertisement = await Advertisement.find();
 
-//     res.status(200).json(advertisement)
-// }
+    res.status(200).json(advertisement)
+}
 
-// exports.getSingleAdvertisement = async (req, res) => {
+exports.getSingleAdvertisement = async (req, res) => {
 
-//     const advertisement = await Advertisement.findById(req.params.id);
+    const advertisement = await Advertisement.findById(req.params.id);
 
-//     if(!advertisement) {
-//         return res.status(404).json({
-//             success: false,
-//             message: 'Advertisement not found'
-//         })
-//     }
+    if(!advertisement) {
+        return res.status(404).json({
+            success: false,
+            message: 'Advertisement not found'
+        })
+    }
 
-//     res.status(200).json({
-//         success: true,
-//         advertisement
-//     })
-// }
+    res.status(200).json({
+        success: true,
+        advertisement
+    })
+}
 
-// exports.updateAdvertisement = async (req, res) => {
+exports.updateAdvertisement = async (req, res) => {
 
-//     let advertisement = await Advertisement.findById(req.params.id);
+    let advertisement = await Advertisement.findById(req.params.id);
   
-//     if(!advertisement) {
-//         return res.status(404).json({
-//             success: false, 
-//             message: 'Advertisment not found'
-//         })
-//     }
+    if(!advertisement) {
+        return res.status(404).json({
+            success: false, 
+            message: 'Advertisment not found'
+        })
+    }
   
-//     advertisement = await Advertisement.findByIdAndUpdate(req.params.id, ({
-//         ad_name: req.body.ad_name,
-//         image: req.file.path
+    advertisement = await Advertisement.findByIdAndUpdate(req.params.id, ({
+        ad_name: req.body.ad_name,
+        image: req.file.path
         
-//       }), {
-//         new: true,
-//         runValidators: true,
-//         useFindAndModify: false
-//     });
+      }), {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
   
-//     res.status(200).json({
-//         success: true,
-//         advertisement
-//     })
+    res.status(200).json({
+        success: true,
+        advertisement
+    })
   
-//   }
+  }
 
 
-// exports.deleteAdvertisement = async (req, res) => {
+exports.deleteAdvertisement = async (req, res) => {
 
-//     const advertisement = await Advertisement.findByIdAndDelete(req.params.id);
+    const advertisement = await Advertisement.findByIdAndDelete(req.params.id);
 
-//     if(!advertisement) {
-//         return res.status(404).json({
-//             success: false,
-//             message: 'Advertisement not found'
-//         })
-//     }
+    if(!advertisement) {
+        return res.status(404).json({
+            success: false,
+            message: 'Advertisement not found'
+        })
+    }
 
-//     await advertisement.remove();
+    await advertisement.remove();
 
-//     res.status(200).json({
-//         success: true,
-//         message: 'Advertisement is deleted'
-//     })
+    res.status(200).json({
+        success: true,
+        message: 'Advertisement is deleted'
+    })
 
-// }
+}
