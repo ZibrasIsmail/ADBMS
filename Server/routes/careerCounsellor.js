176ff0db386181735a6ea301cd7712cee0d72a5e
Si-Lunsi <63093719+Si-Lunsi@users.noreply.github.com>
2021-11-28 20:50:01 +0530

    Update careerCounsellor.js

--- Server/routes/careerCounsellor.js
+++ Server/routes/careerCounsellor.js
@@ -1,21 +1,21 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-// const { newCareerCounsellor,
-//         getCareerCounsellor,
-//         deleteCareerCounsellor,
-//         updateCareerCounsellor
+const { newCareerCounsellor,
+        getCareerCounsellor,
+        deleteCareerCounsellor,
+        updateCareerCounsellor
 
-// } = require('../controllers/careerCounsellorController');
+} = require('../controllers/careerCounsellorController');
 
-// const parser = require("../middleware/cloudinary.config");
+const parser = require("../middleware/cloudinary.config");
 
-// router.post('/careercounsellor/new', parser.single("image"), newCareerCounsellor);
+router.post('/careercounsellor/new', parser.single("image"), newCareerCounsellor);
 
-// router.route('/careercounsellor').get(getCareerCounsellor);
+router.route('/careercounsellor').get(getCareerCounsellor);
 
-// router.put('/careercounsellor/update/:id', parser.single("image"), updateCareerCounsellor);
+router.put('/careercounsellor/update/:id', parser.single("image"), updateCareerCounsellor);
 
-// router.route('/careercounsellor/delete/:id').delete(deleteCareerCounsellor);
+router.route('/careercounsellor/delete/:id').delete(deleteCareerCounsellor);
 
-// module.exports = router;
+module.exports = router;
