0816f537748a7c2d9896c192fdbaa2a5e096f1da
AdshayaniPirapaharan <69112755+AdshayaniPirapaharan@users.noreply.github.com>
2021-11-28 22:13:03 +0530

    Update advertisement.js

--- Server/models/advertisement.js
+++ Server/models/advertisement.js
@@ -1,18 +1,18 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const AdvertisementSchema = new mongoose.Schema({
-//   ad_name: {
-//     type: String,
-//     required: [true, "Enter the Advertisement name"],
-//     trim: true,
-//   },
-//   creatorId: {
-//     type: String,
-//     required: [true, "creatorId required"],
-//   },
-//   image: {
-//     type: String,
-//   },
-// });
+const AdvertisementSchema = new mongoose.Schema({
+  ad_name: {
+    type: String,
+    required: [true, "Enter the Advertisement name"],
+    trim: true,
+  },
+  creatorId: {
+    type: String,
+    required: [true, "creatorId required"],
+  },
+  image: {
+    type: String,
+  },
+});
 
-// module.exports = mongoose.model("Advertisement", AdvertisementSchema);
+module.exports = mongoose.model("Advertisement", AdvertisementSchema);
